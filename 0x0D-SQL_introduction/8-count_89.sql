ed61fa116d64b84a2a0cf10fa4d8396b3a496853
kiru19 <kirubels1994@gmail.com>
2022-09-07 00:28:13 -0700

    sql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- Displays the number of records
 -- Query to display the number of records with id = 89 in a table
-SELECT *FROM first_table WHERE id=89;
+SELECT COUNT(*) as id FROM first_table WHERE id=89;
